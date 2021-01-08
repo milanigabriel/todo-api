0277905caf1a21cd9659a66b877d4dde02c16bc7
MilaniGabriel <gabriel.milani@fatec.sp.gov.br>
2021-01-08 10:09:01 -0300

    verbs

---
+++ 02_RestWithASPNET_Verbs/RestWithASPNET/RestWithASPNET/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{value}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber, string value)
+        {
+
+            return BadRequest("Invalid Input");
+        }
+    }
+}
